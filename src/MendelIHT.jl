410e64ead35c79477e8be41f264c28a1792d6f5a
Unknown <benchu99@hotmail.com>
2019-06-28 19:49:05 -0700

    trying to fix linkinv not defined error 7

--- src/MendelIHT.jl
+++ src/MendelIHT.jl
@@ -4,8 +4,7 @@ module MendelIHT
 
 	import Distances: euclidean, chebyshev, sqeuclidean
 	import Base.show
-	import GLM.checky
-	# import GLM: glmvar, mueta, fit, linkinv, Link, GeneralizedLinearModel, devresid, checky, canonicallink
+	import GLM: glmvar, mueta, fit, linkinv, Link, GeneralizedLinearModel, devresid, checky, canonicallink
 
 	using GLM
 	using SnpArrays
