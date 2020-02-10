165ca03918b108b37907b21bb641bf9bf32d2da8
Unknown <benchu99@hotmail.com>
2020-02-10 13:43:17 -0800

    updated make and toml

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,7 @@ makedocs(
     clean = true,
     pages = [
         "Home"            => "index.md",
+        "What is IHT?"    => "man/introduction.md",
         "Getting Started" => "man/getting_started.md",
         "Examples"        => "man/examples.md",
         "Contributing"    => "man/contributing.md",
